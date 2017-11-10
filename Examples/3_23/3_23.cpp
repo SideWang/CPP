b925b3e9e8b1600638145b3a9c99d4fa57c8c15e
SideWang <285798010@qq.com>
2017-11-11 02:26:24 +0800

    增加了用迭代法求平方根3-23目录

--- Examples/3_23/3_23.cpp
+++ Examples/3_23/3_23.cpp
@@ -1,21 +1,24 @@
-// 3_23.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
+#include <cmath>
 using namespace std;
 int N = 5;
-float f(int a)
+float f(double a,double r)
 {
-	if (N <= 1)
-		return 1;
-	return (f(N--) + a/f(N--))/2;
+	static double temp;
+	temp = r;
+	r = (r + (a / r))/2;
+	if (abs(temp - r) < 0.00001)
+		return r;
+	f(a,r);
 }
 int main(int argc, char* argv[])
 {
-	cout << f(2) << endl;
-
-	printf("Hello World!\n");
+	double a;
+	cout << "Please enter a numble." << endl;
+	cin >> a;
+	cout << f(a,a/2) << endl;
 	return 0;
 }
 
